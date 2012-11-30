052551d3c79409bdd4587c13cddb5bbced81483b
tnterdan <tnterdan@gmail.com>
2012-11-29 20:18:46 -0500

    Initial commit

---
+++ src/com/battlegame/MainActivity.java
@@ -0,0 +1,75 @@
+package com.battlegame;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+ 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	// Client vars
+	protected Button connectBtn;
+	protected Button hostBtn;
+	protected TextView tv;
+	protected TextView ipAddress;
+	protected TextView port;
+	protected Socket socket;
+
+	// Server vars
+	ServerSocket ss = null;
+	String mClientMsg = "";
+	Thread myCommsThread = null;
+	   
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		connectBtn = (Button) findViewById(R.id.sendButton);
+		hostBtn = (Button) findViewById(R.id.hostButton);
+		tv = (TextView) findViewById(R.id.myTextView);
+		ipAddress = (TextView) findViewById(R.id.ipAddress);
+		port = (TextView) findViewById(R.id.port);
+		connectBtn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				CharSequence connectionString = "Waiting to connect to " + ipAddress.getText();
+				Toast.makeText(v.getContext(), connectionString, Toast.LENGTH_LONG).show();
+
+		  	    try {
+		 	         InetAddress serverAddr = InetAddress.getByName(ipAddress.getText().toString());		 	       
+		 	         socket = new Socket(serverAddr, Integer.parseInt(port.getText().toString()));
+			  	    	connectionString = "Connection successful";
+			  	    	Toast.makeText(v.getContext(), connectionString, Toast.LENGTH_LONG).show();
+		  	    } catch (UnknownHostException e1) {
+		  	    	connectionString = "Unknown host";
+			        Toast.makeText(v.getContext(), connectionString, Toast.LENGTH_LONG).show();
+			        e1.printStackTrace();
+		  	    } catch (IOException e1) {
+		  	    	connectionString = "Could not connect";
+		  	    	Toast.makeText(v.getContext(), connectionString, Toast.LENGTH_LONG).show();
+		 	        e1.printStackTrace();
+		 	    }
+		  	    catch (NumberFormatException e1) {
+		  	    	connectionString = "Invalid IP Address/Port";
+		  	    	Toast.makeText(v.getContext(), connectionString, Toast.LENGTH_LONG).show();
+		 	        e1.printStackTrace();
+		  	    }
+	         }
+		});
+
+        hostBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent nextScreen = new Intent(getApplicationContext(), HostActivity.class);
+                startActivity(nextScreen);
+            }
+        });
+    }
+}
